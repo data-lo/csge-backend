22901355e24e92fb9a2b80b4e8afd81c14c6c74a
angelcepeda <jangelcepeda@protonmail.com>
2024-10-16 10:59:27 -0600

    Configuracion de conexión DB en Render
    Configuración de varaible de entorno en env.development.cloud para el URL de conexion con SSL activo

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,11 +20,8 @@ import { ConfigModule } from '@nestjs/config';
     }),
     TypeOrmModule.forRoot({
       type:"postgres",
-      host:process.env.DB_HOST,
-      port:Number(process.env.DB_PORT),
-      username:process.env.DB_USERNAME,
-      password:process.env.DB_PASSWORD,
-      database:process.env.DB_NAME,
+      url:process.env.DATABASE_URL,
+      ssl:true,
       synchronize:true,
       autoLoadEntities:true
       }),
