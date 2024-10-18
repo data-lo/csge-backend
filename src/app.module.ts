5fa54188435020a43571543a1abe2ee1538a493c
angelcepeda <jangelcepeda@protonmail.com>
2024-10-18 13:24:19 -0600

    Entorno de dev en cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,33 +11,27 @@ import { TextosModule } from './configuracion/textos/textos.module';
 import { RespFirmaModule } from './configuracion/resp_firma/resp_firma.module';
 import { ColoresModule } from './configuracion/colores/colores.module';
 import { ConfigModule } from '@nestjs/config';
-import { SeedModule } from './seed/seed.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development.local',
+      envFilePath: '.env.cloud',
       isGlobal:true,
     }),
     TypeOrmModule.forRoot({
       type:"postgres",
-      host:process.env.DB_HOST,
-      port:Number(process.env.DB_PORT),
-      username:process.env.DB_USERNAME,
-      password:process.env.DB_PASSWORD,
-      database:process.env.DB_NAME,
+      url:process.env.DATABASE_URL,
+      ssl:true,
       synchronize:true,
       autoLoadEntities:true
       }),
-    
-    UsuariosModule,
+    UsuariosModule, 
     PuestosModule, 
     DepartamentosModule, 
     IvaModule, 
     TextosModule, 
     RespFirmaModule, 
-    ColoresModule, SeedModule, AuthModule],
+    ColoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
