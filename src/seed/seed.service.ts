c8a29915797181ec69f63c8699bd8459109aa9f2
angelcepeda <jangelcepeda@protonmail.com>
2024-10-18 13:39:36 -0600

    seed modificada, insertar puestos y departamentos

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -20,7 +20,8 @@ export class SeedService {
   ){}
   
   async seed(){
-    this.insertarUsuarios();
+    this.insertarDepartamentos()
+    this.insertarPuestos()
   }
 
   async insertarPuestos(){
