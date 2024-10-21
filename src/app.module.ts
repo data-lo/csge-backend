a8f1b4bf59740560269903219be5f7060f84fa87
angelcepeda <jangelcepeda@protonmail.com>
2024-10-21 17:12:31 -0600

    Main Moudle Actualizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { UsuariosModule } from './administracion/usuarios/usuarios.module';
 import { PuestosModule } from './administracion/puestos/puestos.module';
 import { DepartamentosModule } from './administracion/departamentos/departamentos.module';
@@ -14,20 +13,24 @@ import { ConfigModule } from '@nestjs/config';
 import { SeedModule } from './seed/seed.module';
 import { AuthModule } from './auth/auth.module';
 import { ImagenModule } from './configuracion/imagen/imagen.module';
-import { env } from 'process';
+import { CaracteristicasModule } from './catalogos/caracteristicas/caracteristicas.module';
+import { ContratosModule } from './contratos/contratos/contratos.module';
+import { ContratosModificatoriosModule } from './contratos/contratos_modificatorios/contratos_modificatorios.module';
+import { DimensionesModule } from './catalogos/dimensiones/dimensiones.module';
+import { FormatosModule } from './catalogos/formatos/formatos.module';
+import { ImpresionesModule } from './catalogos/impresiones/impresiones.module';
+import { LongitudesModule } from './catalogos/longitudes/longitudes.module';
+import { TiemposModule } from './catalogos/tiempos/tiempos.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development',
+      envFilePath: '.env.cloud',
       isGlobal:true,
     }),
     TypeOrmModule.forRoot({
       type:"postgres",
-      database:process.env.DB_NAME,
-      host:process.env.DB_HOST,
-      username:process.env.DB_USERNAME,
-      password:process.env.DB_PASSWORD,
+      url:process.env.DATABASE_URL,
       ssl:true,
       synchronize:true,
       autoLoadEntities:true
@@ -41,7 +44,16 @@ import { env } from 'process';
     ColoresModule, 
     SeedModule, 
     AuthModule, 
-    ImagenModule],
+    ImagenModule,
+    ContratosModule,
+    ContratosModificatoriosModule,
+    CaracteristicasModule,
+    DimensionesModule,
+    FormatosModule,
+    ImpresionesModule,
+    LongitudesModule,
+    TiemposModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
