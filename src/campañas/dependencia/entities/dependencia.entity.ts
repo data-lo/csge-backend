930dec835faaf80c9f6223474c31548d2d391b39
angelcepeda <jangelcepeda@protonmail.com>
2024-10-26 18:10:50 -0600

    Dependencia DTO y Entidad lista

--- src/campañas/dependencia/entities/dependencia.entity.ts
+++ src/campañas/dependencia/entities/dependencia.entity.ts
@@ -1,1 +1,15 @@
-export class Dependencia {}
+import { Column, Entity, Generated, PrimaryColumn } from "typeorm";
+
+@Entity('Dependencia')
+export class Dependencia {
+    
+    @Generated('uuid')
+    @PrimaryColumn('uuid')
+    id:string;
+
+    @Column({
+        name:'nombre_dependencia',
+        nullable:false
+    })
+    nombre:string;
+}
