2ab60e6ead980f348829d131827587a069cac447
angelcepeda <jangelcepeda@protonmail.com>
2024-11-15 13:37:07 -0600

    Sin validacion de tupo de imagen

--- src/helpers/fileFilter.ts
+++ src/helpers/fileFilter.ts
@@ -1,11 +1,13 @@
 export const fileFilter = (req:Express.Request,file:Express.Multer.File, callback:Function) => {
  
     if(!file) return callback(new Error('No se encuentra imagen'), false );
-    const fileExtension = file.mimetype.split('/')[1];
-    const validExtensions = ['jpg','jpeg','png'];
-
-    if( validExtensions.includes(fileExtension)){
-        return callback(null,true);
-    }
-    callback(null,false);
+    
+    //const fileExtension = file.mimetype.split('/')[1];
+    //const validExtensions = ['jpg','jpeg','png'];
+//
+    //if( validExtensions.includes(fileExtension)){
+    //    return callback(null,true);
+    //}
+    
+    callback(null,true);
 }
