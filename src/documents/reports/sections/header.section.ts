8dcbc3448c1a8d15c7a9d6831a4c8c0d8c4ea755
angelcepeda <jangelcepeda@protonmail.com>
2024-11-26 14:46:07 -0600

    fix: Try catch y validación de la ruta de la imagen para el encabezado en fileSystem

--- src/documents/reports/sections/header.section.ts
+++ src/documents/reports/sections/header.section.ts
@@ -1,85 +1,91 @@
-import { readdirSync } from "fs"
-import { join } from "path"
-import { Content } from "pdfmake/interfaces";
-import sharp from "sharp";
+import { readdirSync } from 'fs';
+import { join } from 'path';
+import { Content } from 'pdfmake/interfaces';
+import sharp from 'sharp';
+import { handleExeptions } from 'src/helpers/handleExceptions.function';
 
 interface HeaderOptions {
-    showTitle:boolean;
-    showLogo:boolean;
-    textoEncabezado:string;
-    folio:string
+  showTitle: boolean;
+  showLogo: boolean;
+  textoEncabezado: string;
+  folio: string;
 }
 
 const refreshRoute = () => {
-    const rutaDeCarga = join(__dirname,'../../../../static/uploads/imagen')
+  try {
+    const rutaDeCarga = join(__dirname, '../../../../static/uploads/imagen');
     const imagen = readdirSync(rutaDeCarga);
-    const path = join(rutaDeCarga,imagen[0]);
-    return path
-}
-
-const getImageDimensions = async (imagePath: string): Promise<{ width: number; height: number }> => {
-    const metadata = await sharp(imagePath).metadata();
-    return {
-        width: metadata.width || 0,
-        height: metadata.height || 0
-    };
+    const path = join(rutaDeCarga, imagen[0]);
+    if (!path) return null;
+    return path;
+  } catch (error) {
+    handleExeptions(error);
+  }
 };
 
+const getImageDimensions = async (
+  imagePath: string,
+): Promise<{ width: number; height: number }> => {
+  const metadata = await sharp(imagePath).metadata();
+  return {
+    width: metadata.width || 0,
+    height: metadata.height || 0,
+  };
+};
 
 const dividirTextoPorEspacios = (texto: string, longitud: number): string => {
-    const palabras = texto.split(" "); // Dividimos el texto en palabras
-    let resultado = "";
-    let lineaActual = "";
+  const palabras = texto.split(' '); // Dividimos el texto en palabras
+  let resultado = '';
+  let lineaActual = '';
 
-    for (const palabra of palabras) {
-        if ((lineaActual + palabra).length > longitud) {
-            resultado += lineaActual.trim() + "\n"; // Agrega la línea acumulada al resultado
-            lineaActual = ""; // Reinicia la línea actual
-        }
-        lineaActual += palabra + " "; // Agrega la palabra a la línea actual
+  for (const palabra of palabras) {
+    if ((lineaActual + palabra).length > longitud) {
+      resultado += lineaActual.trim() + '\n'; // Agrega la línea acumulada al resultado
+      lineaActual = ''; // Reinicia la línea actual
     }
-    resultado += lineaActual.trim();
+    lineaActual += palabra + ' '; // Agrega la palabra a la línea actual
+  }
+  resultado += lineaActual.trim();
 
-    return resultado;
+  return resultado;
 };
 
-export const headerSection = async (options:HeaderOptions):Promise<Content> => {
-    
-    let logo:Content = null;
-    
-    const {showTitle, showLogo, textoEncabezado, folio} = options;
-    
-    if(showLogo){
-        const imagePath = refreshRoute();
-        const {width, height} = await getImageDimensions(imagePath);
+export const headerSection = async (
+  options: HeaderOptions,
+): Promise<Content> => {
+  let logo: Content = null;
 
-        const maxWidth = 150;
-        const maxheight  = 100;
-        const scale = Math.min(maxWidth/width, maxheight/height);
+  const { showTitle, showLogo, textoEncabezado, folio } = options;
 
-        logo = {
-            image:imagePath,
-            width: width * scale,
-            height: height * scale,
-            alignment: 'left',
-            margin:[30,20,10,10]
-        };
-        
-    }
-    
-    const headerText:Content = {
-        text:`${dividirTextoPorEspacios(textoEncabezado,50)}\n${folio}`,
-        alignment:'center',
-        font:'Poppins',
-        bold:true,
-        margin: [30,30,0,20],
-    }
+  if (showLogo) {
+    const imagePath = refreshRoute();
+    if (imagePath) {
+      const { width, height } = await getImageDimensions(imagePath);
 
-    return {
-        columns: [
-            showLogo ? logo : null,
-            showTitle ? headerText: null
-        ],
-        margin:[0,0,0,20]
+      const maxWidth = 150;
+      const maxheight = 100;
+      const scale = Math.min(maxWidth / width, maxheight / height);
+
+      logo = {
+        image: imagePath,
+        width: width * scale,
+        height: height * scale,
+        alignment: 'left',
+        margin: [30, 20, 10, 10],
+      };
     }
-}
+  }
+
+  const headerText: Content = {
+    text: `${dividirTextoPorEspacios(textoEncabezado, 50)}\n${folio}`,
+    alignment: 'center',
+    font: 'Poppins',
+    bold: true,
+    margin: [30, 30, 0, 20],
+  };
+
+  return {
+    columns: [showLogo ? logo : null, showTitle ? headerText : null],
+    margin: [0, 0, 0, 20],
+  };
+};
