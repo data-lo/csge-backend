a30eb1bad9d20eb593c05bde64ea5fa1bbe9ed18
angelcepeda <jangelcepeda@protonmail.com>
2024-12-09 15:32:03 -0600

    enhancement: setting de allowedOrigins, local host, deploy de frontend, y server de firmamex

--- src/allowedOrigins.ts
+++ src/allowedOrigins.ts
@@ -1,5 +1,5 @@
 export const allowedOrigins = [
-    '*',
-    //'localhost',
-    //'https://csge-frontend-349036387168.us-central1.run.app/',
+    'localhost',
+    'https://csge-frontend-349036387168.us-central1.run.app/',
+    'gestorescpd.chihuahua.gob.mx'
 ]
