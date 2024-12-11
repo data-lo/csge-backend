2a5830221a6b01a95b58d3b69b4a4d372776e128
angelcepeda <jangelcepeda@protonmail.com>
2024-12-11 14:09:06 -0600

    fix: objeto internal server error, para consistencia en mensajes de error hacia el front

--- src/filters/all-exceptions.filter.ts
+++ src/filters/all-exceptions.filter.ts
@@ -17,7 +17,13 @@ export class AllExceptionsFilter extends BaseExceptionFilter{
     catch(exception: any, host: ArgumentsHost): void {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
-        const request = ctx.getResponse<Request>();
+        const request = ctx.getRequest<Request>();
+
+        const internalServererrrorResponse = {
+            message:"Inernal Server Error",
+            error:'Error',
+            statusCode:500
+        }
 
         const myResponseObj: MyResponseObj = {
             statusCode:500,
@@ -29,15 +35,17 @@ export class AllExceptionsFilter extends BaseExceptionFilter{
         if(exception instanceof HttpException){
             myResponseObj.statusCode = exception.getStatus(),
             myResponseObj.response = exception.getResponse()
+        
         }else{
             myResponseObj.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
-            myResponseObj.response = 'Internal Server Error'
+            myResponseObj.response = internalServererrrorResponse
         };
 
         response.status(myResponseObj.statusCode)
                 .json(myResponseObj)
 
         this.logger.error(myResponseObj.response, AllExceptionsFilter.name);
+        
         super.catch(exception, host);
     };
 }
