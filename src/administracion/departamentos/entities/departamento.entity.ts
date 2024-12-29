4cdbc797f848d137fbfc6bd43ae5ceabbd1e42e9
Juan Angel Cepeda <jangelcepeda@protonmail.com>
2024-12-28 23:17:47 -0600

    feature: se actualizan las entidades y los servicios para ajustar los campos de creadoEn y actualizadoEn

--- src/administracion/departamentos/entities/departamento.entity.ts
+++ src/administracion/departamentos/entities/departamento.entity.ts
@@ -1,36 +1,47 @@
-import { localeTimeFormatter } from "src/helpers/localeTimeZoneFormater.function";
-import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, Generated, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { localeTimeFormatter } from 'src/helpers/localeTimeZoneFormater.function';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('departamentos')
 export class Departamento {
-    @PrimaryColumn("uuid")
-    @Generated("uuid")
-    id:string;
+  @PrimaryColumn('uuid')
+  @Generated('uuid')
+  id: string;
 
-    @Column({
-        length:50,
-        unique:true
-    })
-    nombre:string;
+  @Column({
+    length: 50,
+    unique: true,
+  })
+  nombre: string;
 
-    @CreateDateColumn({
-        name:'creado_en'
-    })
-    creadoEn:Date;
+  @CreateDateColumn({
+    name: 'creado_en',
+  })
+  creadoEn: Date;
 
-    @UpdateDateColumn({
-        name:'actualizado_en'
-    })
-    actualizadoEn:Date;
+  @UpdateDateColumn({
+    name: 'actualizado_en',
+  })
+  actualizadoEn: Date;
 
-    @BeforeInsert()
-    localeTimeZoneInsert(){
-        this.creadoEn = localeTimeFormatter(this.creadoEn);
-        this.actualizadoEn = localeTimeFormatter(this.actualizadoEn);
-    }
-    
-    @BeforeUpdate()
-    localeTimeZoneUpdate(){
-        this.actualizadoEn = localeTimeFormatter(this.actualizadoEn);
-    }
+  @BeforeInsert()
+  localeTimeZoneInsert() {
+    const value = new Date();
+    this.creadoEn = localeTimeFormatter(value);
+    this.actualizadoEn = localeTimeFormatter(value);
+  }
+
+  @BeforeUpdate()
+  localeTimeZoneUpdate() {
+    const value = new Date();
+    this.actualizadoEn = localeTimeFormatter(value);
+  }
 }
